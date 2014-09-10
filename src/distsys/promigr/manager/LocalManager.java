bff1a6ab15e22cd2561ca42110610dc6dff7b4d6
Neil Dhruva <neildhruva@gmail.com>
2014-09-10 16:26:27 -0400

    local manager exception catching

--- src/distsys/promigr/manager/LocalManager.java
+++ src/distsys/promigr/manager/LocalManager.java
@@ -15,17 +15,33 @@ public class LocalManager
 	public static ConcurrentMap<String, ThreadObject> threadMap;
 	private final static int serverPort = 50000;
 	
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         threadMap = new ConcurrentHashMap<String, ThreadObject>();
         	
         //create server socket
         //TODO: remove "serverPort" and write 50000?
-        ServerSocket serverSocket = new ServerSocket(serverPort);          
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(serverPort);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            //System.out.println("Can't create local server.");
+        }          
         while(true) {
-            Socket clientSocket = serverSocket.accept();
-            LocalManagerThread lmthread = new LocalManagerThread(clientSocket, threadMap, serverPort);
-            Thread thread = new Thread(lmthread);
-            thread.start();            
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            }
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                //System.out.println("Can't accept connections.");
+            }
+            if(clientSocket != null) {
+                LocalManagerThread lmthread = new LocalManagerThread(clientSocket, threadMap, serverPort);
+                Thread thread = new Thread(lmthread);
+                thread.start();
+            }
         }
         
     }
